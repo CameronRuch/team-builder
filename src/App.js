4a027a5839dec0a4537c4a53833e3e449d5122fa
CameronRuch <cameron.ruch@yahoo.com>
2022-02-22 22:08:58 -0600

    created app using npx and created Form.js component

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import React from 'react';
+import { useState } from 'react';
+
+function App() {
+
+
+
+  return (
+    <div className="App">
+     
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Form.js
@@ -0,0 +1,12 @@
+
+import React from "react";
+
+const Form = (props) => {
+
+    return (
+
+    )
+
+}
+
+export default Form;
