f42e38846335c0872d18b9ccd2f106c3736a63d2
CameronRuch <cameron.ruch@yahoo.com>
2022-02-22 22:44:57 -0600

    added name, role, email, and submit inputs to Form.js, added onSubmit to send new inputs to Form and resets inputs to original state. Added onChange to update state when new input is added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import './App.css';
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
+import Form from './Components/Form';
+
 
 function App() {
 
+  const [members, setMembers] = useState([]);
+  const [data, setData] = useState({name: '', email: '', role: ''});
 
+  const onSubmit = () => {
+    setMembers([data, ...members]);
+    setData({name: '', email: '', role: ''})
+  }
 
+  const onChange = (name, value) => {
+    setData({...data, [name]: value});
+  }
   return (
     <div className="App">
-     
+
+     <Form member={data} change={onChange} submit={onSubmit}/>
+      
+     {members.map((member, idx) => {
+       return (
+         <div key={idx}>
+           {member.name}, {member.role}, {member.email}
+           </div>
+       )
+     })}
 
     </div>
   );
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -3,7 +3,41 @@ import React from "react";
 
 const Form = (props) => {
 
+    const handleChange = evt => {
+        const { name, value } = evt.target;
+        props.change(name, value)
+    }
+
+   const handleSubmit = evt => {
+        evt.preventDefault();
+        props.submit();
+    }
+
     return (
+        <form onSubmit={handleSubmit}>
+            <label name="name">Name</label>
+            <input
+                placeholder="Enter new member's name"
+                value={props.member.name}
+                name = "name"
+                onChange={handleChange}
+            />
+            <label name="email">Email</label>
+            <input 
+                placeholder="Enter new member's email"
+                value={props.member.email}
+                name="email"
+                onChange={handleChange}
+            />
+            <label name= "role">Role</label>
+            <input
+                placeholder="Enter new member's role"
+                value={props.member.role}
+                name="role"
+                onChange={handleChange}
+            />
+            <input type="submit" value="Create new team." />
+        </form>
 
     )
 
